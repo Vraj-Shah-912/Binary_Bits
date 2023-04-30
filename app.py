60aacd51cd5e298e9433d9c2a6ac79663a3cd9ec
aryan patel <210130107060@gecg28.ac.in>
2023-04-30 11:56:59 +0530

    final submision in hacknuthon nirma

--- app.py
+++ app.py
@@ -7,17 +7,20 @@ from flaskext.mysql import MySQL
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+
 def get_email_list(mysheet):
-    
+
     row = mysheet.col_values(2)
     email_list = row[1:]
     return email_list
+
 
 def compare_emails(l1, l2):
     set1 = set(l1)
     set2 = set(l2)
     not_common_emails = list(set1.symmetric_difference(set2))
     return not_common_emails
+
 
 app = Flask(__name__)
 config = {
@@ -65,7 +68,7 @@ def login():
         password = request.form.get('password')
 
         try:
-            user=auth.sign_in_with_email_and_password(email, password)
+            user = auth.sign_in_with_email_and_password(email, password)
 
             str1 = "select * from faculty where email='"
             cursor.execute(str1+email+"'")
@@ -113,7 +116,6 @@ def signup():
         return render_template('signup.html')
 
 
-
 @app.route('/logout')
 def logout():
     if ('user' in session):
@@ -143,7 +145,7 @@ def forgot():
 def sendmail():
     if request.method == 'POST':
         try:
-            title= request.form.get('title')
+            title = request.form.get('title')
             formlink = request.form.get('link')
             msg = request.form.get('msg')
             department = request.form.get('department')
@@ -165,16 +167,10 @@ def sendmail():
             for i in list:
                 sendemail(i, 'New Form', formlink, msg, name[0])
 
-            sql = "insert into sentmail values('"+department+"',"+semester+",'"+batch+"','"+formlink+"','"+title+"')"
+            sql = "insert into sentmail values('"+department+"'," + \
+                semester+",'"+batch+"','"+formlink+"','"+title+"')"
             cursor.execute(sql)
             conn.commit()
-
-            sq = "insert into sentmail values('"+department+"',"+semester+",'"+batch+"','"+formlink+"','"+title+"')"
-            print(sq)
-
-            cursor.execute(sq)
-            conn.commit()
-
         except:
             flash("something went wrong", "danger")
             return redirect('/sendmail')
@@ -186,16 +182,23 @@ def sendmail():
 
 @app.route('/reminder', methods=['POST', 'GET'])
 def sendreminder():
+    print("in reminder")
     if request.method == 'POST':
+        # if request.form.get("reminder"):
+        print("in if")
         try:
+            print("in try")
             name = session['user']
             googlesheet = request.form.get('googlesheet')
             subsheet = request.form.get('subsheet')
             msg = request.form.get('msg')
-            title=request.form.get('title')            
-            myscope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+            title = request.form.get('title')
+            print(title+msg)
+            myscope = ['https://spreadsheets.google.com/feeds',
+                        'https://www.googleapis.com/auth/drive']
 
-            mycreds = ServiceAccountCredentials.from_json_keyfile_name('secret-medium-385219-771c7ef500e1.json', myscope)
+            mycreds = ServiceAccountCredentials.from_json_keyfile_name(
+                'secret-medium-385219-771c7ef500e1.json', myscope)
 
             myclient = gspread.authorize(mycreds)
 
@@ -205,42 +208,97 @@ def sendreminder():
             mysheet = myclient.open(sh1).worksheet(sh2)
             # l1 = ["e1@example.com", "e2@example.com", "e3@example.com"]
             l2 = get_email_list(mysheet)
-            print(l2)
-            # print(l2)
 
             cursor = conn.cursor()
 
-            s1="select * from sentmail where title='"
+            s1 = "select * from sentmail where title='"
             cursor.execute(s1+title+"'")
             result = cursor.fetchone()
             str1 = "select email from student where department = '"
             str3 = "' and semester = "
             str5 = " and batch = '"
             str10 = str1+result[0]+str3+str(result[1])+str5+result[2]+"'"
+            print(result)
             cursor.execute(str10)
             items = cursor.fetchall()
             l1 = []
             for item in items:
                 # int(item[0])
                 l1.append(item[0])
-            print(l1)
             l3 = compare_emails(l1, l2)
-            print(l3)
             for i in l3:
                 sendemail(i, 'Reminder', result[3], msg, name[0])
-        except:
-            flash("user not found", "danger")
-            return redirect('/forgot')
-        flash("reset email has send ", "success")
-        return redirect('/')
-    else:
-        cursor1=conn.cursor()
-        sq = "SELECT title FROM sentmail"
-        cursor1.execute(sq)
-        result = cursor1.fetchall()
-        print(result)
-        return render_template('sendreminder.html',result=result)
 
+            print(l3)
+            flash("reminder mail has send ", "success")
+            return render_template('sendreminder.html',l3=l3)    
+        
+        except:
+            flash("Somthing went wrong !!", "danger")
+            return redirect('/reminder')
+    
+
+        # if request.form.get("followup"):
+        #     try:
+        #         googlesheet = request.form.get('googlesheet')
+        #         subsheet = request.form.get('subsheet')
+        #         msg = request.form.get('msg')
+        #         title = request.form.get('title')
+        #         print(title)
+        #         s1 = "select * from sentmail where title='"
+        #         cursor.execute(s1+title+"'")
+        #         result = cursor.fetchall()
+        #         print(result)
+        #         str1 = "select email from student where department = '"
+        #         str3 = "' and semester = "
+        #         str5 = " and batch = '"
+        #         str10 = str1+result[0]+str3+str(result[1])+str5+result[2]+"'"
+        #         cursor.execute(str10)
+        #         items = cursor.fetchall()
+        #         print(str10)
+        #         # print(items)
+        #         return render_template('followup.html', items=items)
+        #     except:
+        #         flash("somthing went wrong", "danger")
+        #         return redirect('/reminder')
+        # else:
+        #     return render_template('followup.html')
+    
+    cursor1 = conn.cursor()
+    sq = "SELECT title FROM sentmail"
+    cursor1.execute(sq)
+    result = cursor1.fetchall()
+    return render_template('sendreminder.html', result=result)
+
+
+@app.route('/followup', methods=['POST', 'GET'])
+def followup():
+    if request.method == 'POST':
+        try:
+            googlesheet = request.form.get('googlesheet')
+            subsheet = request.form.get('subsheet')
+            msg = request.form.get('msg')
+            title = request.form.get('title')
+            print(title)
+            s1 = "select * from sentmail where title='"
+            cursor.execute(s1+title+"'")
+            result = cursor.fetchall()
+            print(result)
+            str1 = "select email from student where department = '"
+            str3 = "' and semester = "
+            str5 = " and batch = '"
+            str10 = str1+result[0]+str3+str(result[1])+str5+result[2]+"'"
+            cursor.execute(str10)
+            items = cursor.fetchall()
+            print(str10)
+            # print(items)
+            return render_template('followup.html', items=items)
+
+        except:
+            flash("somthing went wrong", "danger")
+            return redirect('/reminder')
+    else:
+        return render_template('followup.html')
 
 
 app.run(debug=True, port=5001)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-@app.route('/sendmail',method=['POST','GET'])
-# def sendmail():
-#     if request.method == 'POST':
-#         try:
-#             formlink = request.form.get('formlink')
-#         except:
-#             flash("user not found","danger")
-#             return redirect('/forgot')
-#         flash("reset email has send " ,"success")
-#         return redirect('/')
-#     else:
-#         return 0
+print(str10)
